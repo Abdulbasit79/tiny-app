2f3e85d9909c28ec8fe974bde3a1008bbce3a788
Abdulbasit79 <Abdulbasit700@outlook.com>
2021-12-07 23:30:41 +0000

    update

---
+++ express_server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs");
+
+function generateRandomString() {
+  let random = Math.random().toString(36).slice(6);
+  console.log(random);
+  return random;
+}
+console.log(generateRandomString('youtube.com'));
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
+  });
+
+  app.get("/hello", (req, res) => {
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  });
+
+  app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+
+  app.get("/u/:shortURL", (req, res) => {
+    const longURL = urlDatabase[req.params.shortURL];
+    res.redirect(longURL);
+  });
+
+  app.post("/urls", (req, res) => {
+    const shortURL = generateRandomString();
+    urlDatabase[shortURL] = req.body.longURL;
+    res.redirect(`urls/${shortURL}`);
+  });
+ 
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
+  app.get("/urls/:shortURL", (req, res) => {
+    const shortURL = req.params.shortURL;
+    let templateVars = { shortURL, longURL: urlDatabase[shortURL]};
+    res.render("urls_show", templateVars);
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+  });
